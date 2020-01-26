69ad97eb6193f589aa6ef6b5c96f40c7ea17d362
Tudor Constantin <tudorconstantin@gmail.com>
2020-01-26 21:36:57 +0200

    load the css from mapbox

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,19 @@
+const backendApi = process.env.API_URL
+  ? `http://${process.env.API_URL}`
+  : "http://localhost:8081";
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  devServer: {
+    proxy: {
+      "^/api": {
+        target: backendApi,
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
+
 }
